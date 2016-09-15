997d120e5295d97f7a8eff873429a70ea79ec5fe
Dominic Brazeel <caboose271@gmail.com>
2016-09-15 15:53:33 -0500

    end of the cleanup?
    probably not.  but here's the rest of what currently seems useless

--- SpotifyRadio/Startup.cs
+++ SpotifyRadio/Startup.cs
@@ -1,14 +1,12 @@
 ﻿using Microsoft.Owin;
 using Owin;
 
-[assembly: OwinStartupAttribute(typeof(SpotifyRadio.Startup))]
 namespace SpotifyRadio
 {
     public partial class Startup
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
