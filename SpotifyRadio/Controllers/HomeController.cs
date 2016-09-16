d89220c222222f21494e69bdc3fb38b2965040a1
Dominic Brazeel <caboose271@gmail.com>
2016-09-15 19:13:11 -0500

    style and web config update

--- SpotifyRadio/Controllers/HomeController.cs
+++ SpotifyRadio/Controllers/HomeController.cs
@@ -1,28 +1,9 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using SpotifyAPI.Web.Auth;
-using SpotifyAPI.Web.Enums;
-using SpotifyAPI.Web.Models;
-using SpotifyRadio.Objects;
-using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace SpotifyRadio.Controllers
 {
     public class HomeController : Controller
-    {
-        private string client_id = "23f299b83b5e45c0ad209144ac4e9c5c";
-        private string redirect_uri = @"http://localhost:27784/";
-        private string scope = "playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-modify-private";
-        
+    {   
         public ActionResult Index()
         {
             return View();
@@ -37,10 +18,10 @@ namespace SpotifyRadio.Controllers
         {
             //get an oauth code
             return Redirect("https://accounts.spotify.com/authorize?" +
-                "client_id=" + client_id +
+                "client_id=" + Settings.ClientId +
                 "&response_type=token" +
-                "&scope=" + scope + 
-                "&redirect_uri=" + redirect_uri);
+                "&scope=" + Settings.Scope + 
+                "&redirect_uri=" + Settings.RedirectUri);
         }
     }
 }
---
+++ SpotifyRadio/Settings.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Configuration;
+
+namespace SpotifyRadio
+{
+    public class Settings
+    {
+        public static string ClientId { get { return WebConfigurationManager.AppSettings["ClientId"]; } }
+        public static string RedirectUri { get { return WebConfigurationManager.AppSettings["RedirectUri"]; } }
+        public static string Scope { get { return WebConfigurationManager.AppSettings["Scope"]; } }
+    }
+}
